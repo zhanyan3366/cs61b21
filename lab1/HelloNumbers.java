ced5494d476ddda170d5d0107cacd25a4b1e40c2
zhanyan3366 <905118181@qq.com>
2022-07-10 18:53:28 +0800

    111

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -2,10 +2,10 @@ public class HelloNumbers {
     public static void main(String[] args) {
         int x = 1;
         int total = 0;
-        while (x <= 50) {
+        while (x <= 10) {
             System.out.print(total + " ");
             total = total + x;
-            x = x + 11;
+            x = x + 1;
         }
 	}
 } 
